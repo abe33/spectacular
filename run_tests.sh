59fa22f91de964cae1520e363a92e3fb7a72d57d
Cédric Néhémie <cedric.nehemie@gmail.com>
2013-05-26 13:44:13 +0200

    feat(coverall): add coverall communication in travis tests

--- run_tests.sh
+++ run_tests.sh
@@ -1,7 +1,7 @@
 
 if [ $TRAVIS ]
   then
-    istanbul --hook-run-in-context cover bin/spectacular -- --coffee --profile specs/**/*.coffee && cake phantomjs
+    istanbul --hook-run-in-context cover bin/spectacular -- --coffee --profile specs/**/*.coffee && (cat coverage/lcov.info | node_modules/.bin/coveralls) > /dev/null 2>&1 && (echo "\nPhantomJS\n") && cake phantomjs
 else
   cake compile
   bin/spectacular --coffee --profile specs/**/*.coffee
