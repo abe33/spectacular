ccb44bb1b3f1cc342ffa3dd7945449c81b2e7653
Cédric Néhémie <cedric.nehemie@gmail.com>
2013-05-27 21:09:22 +0200

    chore(scripts): use istanbul coverage even in local tests

--- run_tests.sh
+++ run_tests.sh
@@ -4,5 +4,5 @@ if [ $TRAVIS ]
     istanbul --hook-run-in-context cover bin/spectacular -- --coffee --profile specs/**/*.coffee && (cat coverage/lcov.info | node_modules/.bin/coveralls) > /dev/null 2>&1 && (echo "\nPhantomJS\n") && cake phantomjs
 else
   cake compile
-  bin/spectacular --coffee --profile specs/**/*.coffee
+  istanbul --hook-run-in-context cover bin/spectacular bin/spectacular -- --coffee --profile specs/**/*.coffee
 fi
