ecd74c749f555db2bc958859bd3841d53e361126
Cédric Néhémie <cedric.nehemie@gmail.com>
2013-06-07 01:30:01 +0200

    fix($tests): add missing documentation option on travis

--- run_tests.sh
+++ run_tests.sh
@@ -1,7 +1,7 @@
 
 if [ $TRAVIS ]
   then
-    istanbul --hook-run-in-context cover bin/spectacular -- --coffee --profile -- specs/**/*.coffee && (cat coverage/lcov.info | node_modules/.bin/coveralls) > /dev/null 2>&1 && (echo "\nPhantomJS\n") && cake phantomjs
+    istanbul --hook-run-in-context cover bin/spectacular -- --coffee --profile --documentation -- specs/**/*.coffee && (cat coverage/lcov.info | node_modules/.bin/coveralls) > /dev/null 2>&1 && (echo "\nPhantomJS\n") && cake phantomjs
 else
   cake compile
   if [ $COVERAGE ]
