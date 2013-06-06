051d9db6f23281fc98aeb68b00cd85d4ea704b6e
Cédric Néhémie <cedric.nehemie@gmail.com>
2013-06-07 01:27:27 +0200

    feat($tests): use documentation format when running npm test

--- run_tests.sh
+++ run_tests.sh
@@ -1,14 +1,14 @@
 
 if [ $TRAVIS ]
   then
-    istanbul --hook-run-in-context cover bin/spectacular -- --coffee --profile specs/**/*.coffee && (cat coverage/lcov.info | node_modules/.bin/coveralls) > /dev/null 2>&1 && (echo "\nPhantomJS\n") && cake phantomjs
+    istanbul --hook-run-in-context cover bin/spectacular -- --coffee --profile -- specs/**/*.coffee && (cat coverage/lcov.info | node_modules/.bin/coveralls) > /dev/null 2>&1 && (echo "\nPhantomJS\n") && cake phantomjs
 else
   cake compile
   if [ $COVERAGE ]
     then
-      istanbul --hook-run-in-context cover bin/spectacular -- --coffee --profile specs/**/*.coffee
+      istanbul --hook-run-in-context cover bin/spectacular -- --coffee --profile --documentation specs/**/*.coffee
   else
-      bin/spectacular --coffee --profile specs/**/*.coffee
+      bin/spectacular --coffee --profile --documentation specs/**/*.coffee
   fi
 
 fi
