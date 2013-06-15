1ecf8f7741cd7b6ac9c93e480d01dc2fc59208c8
Cédric Néhémie <cedric.nehemie@gmail.com>
2013-06-15 14:48:09 +0200

    docs(pages): update documentation for version 0.0.3

--- previous/v0.0.2/js/pages.js
+++ previous/v0.0.2/js/pages.js
@@ -1,25 +1,6 @@
 (function() {
   $(function() {
-    var afterInstall, hs, pres, toc, tocList;
-    hs = $('h2, h3, h4, h5, h6');
-    afterInstall = false;
-    hs = hs.filter(function() {
-      if (this.textContent === 'Install') {
-        afterInstall = true;
-      }
-      return afterInstall && $(this).parents('.caniuse_static').length === 0;
-    });
-    toc = $('<nav id="toc"><h2>Table Of Content</h2><ul></ul></nav>');
-    tocList = toc.find('ul');
-    hs.each(function() {
-      var content, id, level;
-      level = parseInt(this.nodeName.slice(1));
-      content = this.textContent;
-      id = content.replace(/[^\w]+/g, '-');
-      this.id = id;
-      return tocList.append("<li class='level" + level + "'><a href='#" + id + "'>" + content + "</a></li>");
-    });
-    $('hr').before(toc);
+    var pres;
     $('.lang-coffeescript').each(function() {
       var code, coffee, div, js, pre, toggle;
       pre = $(this).parent();
@@ -32,7 +13,7 @@
       pre.wrap('<div class="code"></div>');
       div = pre.parent();
       div.height(pre.height());
-      div.prepend("<span class='toggle'>      <span class='coffee' data-text='view as js'>view as js</span>      <span class='js' data-text='view as coffee'>view as coffee</span>    </span>");
+      div.prepend("<span class='toggle'>      <span class='coffee'>view as js</span>      <span class='js'>view as coffee</span>    </span>");
       div.append("<pre class='lang-javascript'><code class='lang-javascript'>" + code + "</code></pre>");
       toggle = div.find('.toggle');
       return toggle.click(function() {
