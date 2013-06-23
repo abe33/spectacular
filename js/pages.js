1701421b8a69510621026a1fb00b04708218e127
Cédric Néhémie <cedric.nehemie@gmail.com>
2013-06-23 15:06:34 +0200

    docs(pages): update documentation to version 1.0.0

--- js/pages.js
+++ js/pages.js
@@ -1,6 +1,6 @@
 (function() {
   $(function() {
-    var afterInstall, hs, pres, toc, tocList;
+    var afterInstall, collapseCell, expandCell, hs, pres, toc, tocList, toggleCellExpansion;
     hs = $('h2, h3, h4, h5, h6');
     afterInstall = false;
     hs = hs.filter(function() {
@@ -45,7 +45,7 @@
       });
     });
     pres = $('pre code');
-    return pres.each(function(i, el) {
+    pres.each(function(i, el) {
       var lines, parent, text;
       el = $(el);
       parent = el.parent();
@@ -55,6 +55,84 @@
       return parent.prepend("<ol>" + (lines.map(function(l, i) {
         return "<li>" + (i + 1) + "</li>";
       }).join('')) + "</ol>");
+    });
+    toggleCellExpansion = function(td, bool) {
+      var ellipsis, tr;
+      tr = td.parents('tr');
+      ellipsis = td.find('.ellipsis');
+      if (bool) {
+        ellipsis.height(ellipsis.data('max-height'));
+        if (!tr.hasClass('open')) {
+          return tr.addClass('open');
+        }
+      } else {
+        ellipsis.height(ellipsis.data('min-height'));
+        if (tr.hasClass('open')) {
+          return tr.removeClass('open');
+        }
+      }
+    };
+    expandCell = function(td) {
+      return toggleCellExpansion(td, true);
+    };
+    collapseCell = function(td) {
+      return toggleCellExpansion(td, false);
+    };
+    $('tr').each(function() {
+      var table, tds, tr;
+      tr = $(this);
+      table = tr.parents('table');
+      tr.addClass('no-padding');
+      tds = tr.find('td');
+      tds.each(function() {
+        var newContent, td;
+        td = $(this);
+        newContent = $("<div>" + (td.html()) + "</div>");
+        td.html('');
+        td.append(newContent);
+        if (tr.height() - 27 > 10) {
+          newContent.addClass('ellipsis');
+          if (!table.hasClass('ellipsis')) {
+            table.addClass('ellipsis');
+          }
+        }
+        newContent.attr('data-min-height', 27);
+        newContent.attr('data-max-height', tr.height());
+        return newContent.height(27);
+      });
+      return tr.click(function() {
+        return tr.find('td div.ellipsis').each(function() {
+          var d;
+          d = $(this);
+          if (d.height() === d.data('min-height')) {
+            return expandCell(d.parents('td'));
+          } else {
+            return collapseCell(d.parents('td'));
+          }
+        });
+      });
+    });
+    return $('table').each(function() {
+      var controls, expandAll, table;
+      if ($(this).find('.ellipsis').length === 0) {
+        return;
+      }
+      table = $(this).wrap('<div class="table-wrapper"/>').parent();
+      controls = $('<div class="table-controls"></div>');
+      table.append(controls);
+      expandAll = $('<button class="expand" title="expand/collapse"><i class="icon-collapse"></i><i class="icon-collapse-top"></i></button>');
+      expandAll.click(function() {
+        var expanded;
+        expandAll.toggleClass('expanded');
+        expanded = expandAll.hasClass('expanded');
+        return table.find('.ellipsis').each(function() {
+          var td;
+          td = $(this).parents('td');
+          return toggleCellExpansion(td, expanded);
+        });
+      });
+      controls.append(expandAll);
+      return new spectacular.SlidingObject(controls[0], table[0]);
     });
   });
 
